fa831be5ae476e436e7644424599236df551a323
yuechray <den747f@gmail.com>
2025-04-16 13:55:43 +0300

    add pydantic-settings

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -2,16 +2,20 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
+from src.db.tables import Base
 
 
+SETTINGS = get_app_settings()
+DATABASE_URL = SETTINGS.postgres_dsn
 
 config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option("sqlalchemy.url", DATABASE_URL + "?async_fallback=True")
 
-target_metadata = None
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
